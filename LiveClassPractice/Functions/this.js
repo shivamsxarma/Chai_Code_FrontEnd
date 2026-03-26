70c6e373fe8d62c6c2cdb7b5589bf213bf263eb3
shivam sharma <shivamsxarma@gmail.com>
2026-03-26 16:32:37 +0530

    new update

--- LiveClassPractice/Functions/this.js
+++ LiveClassPractice/Functions/this.js
@@ -24,3 +24,46 @@ const bollywoodFilm = {
 }
 
 console.log(bollywoodFilm.introduce())
+
+const filmDirector = {
+    name:"sanjay leela",
+    cast:["ranveer", "deepika", "priyanka"],
+
+    announceCast(){
+        this.cast.forEach((actor) =>
+        {
+            console.log(`${this.name} introduced ${actor}`)
+        })
+    }
+}
+// filmDirector.announceCast()
+
+const filmSet = {
+    crew:"spot boys",
+    prepareProps(){
+        console.log(`outer this .crew : ${this.crew}` )
+        function arrangeChairs(){
+            console.log((`inner this crew: ${this.crew}`))
+       
+        }
+             arrangeChairs();
+             const arrangeLights = ()=>{
+                console.log(`Arrow this .crew :${this.crew}`)
+             }
+             arrangeLights()
+    }
+}
+
+filmSet.prepareProps();
+
+
+
+const actor = {
+    name:"ranverr",
+    bow(){
+        return`${this.name} take a bow `
+    }
+}
+
+const detachedBow = actor.bow
+console.log(detachedBow())
